af98fdd261064422840386e809e6d448609b80ae
Jace Randolph <jrandolphacliff@gmail.com>
2023-05-31 13:59:59 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import './App.css';
+import ContactCard from "./ContactCard";
 
 const App = () => {
+  const [results, setResults] = useState([]);
+
+  useEffect(() => {
+    fetch('https://randomuser.me/api/?results=10')
+    .then(response => response.json())
+    .then(data => {
+      console.log(data)
+      setResults(data.results)
+    });
+  }, [])
+  
+
   return (
-    <div>This is my first react component!</div>
+    <>
+    {results.map((result, index) => {
+        return(
+          <ContactCard key={index}
+          avatarUrl = {result.picture.large} 
+          name = {result.first} 
+          email = {result.email}
+          age = {result.dob.age}/>
+        )
+      })}
+    </>
   )
 }
 
